e6b3541a3561e285981a22d260907f73069ad174
psyko944 <mehdi.kherboucheidf@gmail.com>
2024-04-26 23:43:58 +0200

    begin builtin

---
+++ mehdi/test/builtins.h
@@ -0,0 +1,11 @@
+#ifndef BUILTINS_H
+# define BUILTINS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+void mini_echo(int ac, char **av);
+
+# endif
---
+++ mehdi/test/main.c
@@ -0,0 +1,7 @@
+#include "builtins.h"
+
+int main(int ac, char **av)
+{
+    mini_echo(ac, av);
+    return (0);
+}
