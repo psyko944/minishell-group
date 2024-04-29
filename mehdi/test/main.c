224b6f49da08b17c49e52092bc06fd6b82f3c192
psyko944 <mehdi.kherboucheidf@gmail.com>
2024-04-30 00:35:31 +0200

    try_builtin_unset_

--- mehdi/test/main.c
+++ mehdi/test/main.c
@@ -1,7 +1,35 @@
 #include "builtins.h"
 
-int main(int ac, char **av)
+static void	print_env(t_env_var *lst)
 {
-    mini_echo(ac, av);
-    return (0);
+	t_env_var *tmp;
+
+	tmp = lst;
+	while (tmp)
+	{
+		printf("%s=%s\n", tmp->key, tmp->content);
+		tmp = tmp->next;
+	}
+}
+
+static void	print_env_sep(t_env_var *lst)
+{
+	t_env_var *tmp;
+
+	tmp = lst;
+	while (tmp)
+	{
+		printf("key = %s\tcontent = %s\n", tmp->key, tmp->content);
+		tmp = tmp->next;
+	}
+}
+
+int main(int ac, char **av, char **envp)
+{
+	t_env_var *new;
+	new = get_env(envp);
+	ft_unset(new, ac, av);
+	print_env(new);
+	//mini_echo(ac, av);
+	return (0);
 }
