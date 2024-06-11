48b3a4b34a9dd9bf4c20d09748e0d65fd0b7de44
Armand Larzilliere <arlarzil@bess-f1r5s4.clusters.42paris.fr>
2024-06-11 18:48:19 +0200

    Temp push, vars will survive !

---
+++ common/vars/concat.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   concat.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arlarzil <arlarzil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 18:19:39 by arlarzil          #+#    #+#             */
+/*   Updated: 2024/06/11 18:34:27 by arlarzil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "../libft/libft.h"
+#include <stdio.h>
+
+static int	get_full_len(char **tab)
+{
+	int	res;
+
+	res = 0;
+	while (*tab)
+	{
+		res += ft_strlen(*tab);
+		++tab;
+	}
+	return (res + 1);
+}
+
+char	*concat_tab(char **tab)
+{
+	char	*res;
+	char	*respt;
+	int		i;
+
+	res = malloc(get_full_len(tab));
+	if (!res)
+		return (perror("malloc"), NULL);
+	i = 0;
+	*res = 0;
+	respt = res;
+	while (tab[i])
+	{
+		ft_strcpy(respt, tab[i]);
+		while (*respt)
+			++respt;
+		i += 1;
+	}
+	return (res);
+}
+/*
+int	main()
+{
+	char	*tab[] = { "coucou ", "les ", "gens!", NULL};
+	char	*str = concat_tab(tab);
+
+	printf("%s\n", str);
+	free(str);
+}*/
---
+++ common/vars/cut.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cut.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arlarzil <arlarzil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 16:12:12 by arlarzil          #+#    #+#             */
+/*   Updated: 2024/06/11 18:34:07 by arlarzil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+#include "../libft/libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+static int	var_count(const char *s)
+{
+	int	res;
+
+	res = 0;
+	while (*s)
+	{
+		printf("Word start: %s\n", s);
+		if (*s == '$')
+		{
+			++s;
+			if (*s == '{')
+				s = ft_strchr(s, '}');
+			if (!s)
+				return (-1);
+			while (*s && *s != ' ' && *s != '$')
+				++s;
+			++res;
+		}
+		else
+		{
+			++res;
+			while (*s && *s != '$')
+				++s;
+		}
+	}
+	return (res);
+}
+
+static int	get_word_len(char *s)
+{
+	int	i;
+
+	i = 1;
+	if (*s == '$')
+	{
+		if (s[1] == '{')
+			return (ft_strchr(s, '}') - s);
+		while (s[i] && s[i] != ' ' && s[i] != '$')
+			i += 1;
+	}
+	else
+		while (s[i] && s[i] != '$')
+			i += 1;
+	return (i);
+}
+
+char	**cut_vars(char *s)
+{
+	char	**res;
+	int		len;
+	int		i;
+	int		w_len;
+
+	len = var_count(s);
+	if (len == -1)
+		return (ft_putstr_fd("Parse error: unclosed var\n", 2), NULL);
+	res = ft_calloc(len + 1, sizeof(char *));
+	printf("%d words\n", len);
+	if (!res)
+		return (perror("malloc"), NULL);
+	i = 0;
+	while (i < len)
+	{
+		w_len = get_word_len(s);
+		printf("%s: len %d\n", s, w_len);
+		res[i] = ft_strndup_e(s, w_len);
+		s += w_len;
+		i += 1;
+	}
+	return (res);
+}
+/*
+int	main(int ac, char **av)
+{
+	char	**tab = cut_vars(av[ac - 1]);
+	char	**pt = tab;
+
+	while (*pt)
+	{
+		printf("%s\n", *pt);
+		free(*pt);
+		++pt;
+	}
+	free(tab);
+}*/
