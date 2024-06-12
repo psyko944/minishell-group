8366d8668cb6d4ba958d163317def792f952123e
Armand Larzilliere <arlarzil@bess-f2r5s10.clusters.42paris.fr>
2024-06-12 19:30:04 +0200

    Fixed tests for var interpretation

--- common/tests/armand/var_inter.c
+++ common/tests/armand/var_inter.c
@@ -1,13 +1,26 @@
-#include "../../parser/variables/variables.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   var_inter.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arlarzil <arlarzil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 18:46:39 by arlarzil          #+#    #+#             */
+/*   Updated: 2024/06/12 19:29:15 by arlarzil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../parser/vars/vars.h"
 #include "../../libft/libft.h"
+#include "../../minishell.h"
 #include <stdio.h>
 
-void	test_var_inter(char *src, char **env, char *expected)
+void	test_var_inter(char *src, t_env_var **env, char *expected)
 {
 	char	*src_inter;
 
 	src_inter = ft_strdup(src);
-	interpret_variables(src);
+	replace_vars(src, env);
 	if (ft_strcmp(src, expected) != 0)
 		printf("KO: expected %s but got %s\n", expected, src_inter);
 	else
