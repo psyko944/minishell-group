c0684215e893699b141e16f5dc2d0830f097d8f1
Armand Larzilliere <arlarzil@bess-f1r3s14.clusters.42paris.fr>
2024-06-21 19:45:12 +0200

    Handling SIGQUIT

--- common/signals.c
+++ common/signals.c
@@ -6,20 +6,27 @@
 /*   By: arlarzil <arlarzil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 15:56:35 by arlarzil          #+#    #+#             */
-/*   Updated: 2024/05/23 15:56:39 by arlarzil         ###   ########.fr       */
+/*   Updated: 2024/06/21 19:44:58 by arlarzil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <readline/readline.h>
 #include <signal.h>
-
+/*
 static void	handle_sigint(int sig)
 {
 	(void)sig;
 }
-
+*/
 static void	handle_sigquit(int sig)
 {
 	(void)sig;
+
+	rl_cleanup_after_signal();
+	rl_free_line_state();
+	rl_on_new_line();
+	rl_replace_line("  ", 0);
+	rl_redisplay();
 	return ;
 }
 
