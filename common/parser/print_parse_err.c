9c595bd03ca3b21765282c84d0d258f5be484602
Armand Larzilliere <arlarzil@bess-f4r5s1.clusters.42paris.fr>
2024-08-07 19:56:42 +0200

    Parser errors now display on stderr

--- common/parser/print_parse_err.c
+++ common/parser/print_parse_err.c
@@ -6,7 +6,7 @@
 /*   By: arlarzil <arlarzil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 17:22:22 by arlarzil          #+#    #+#             */
-/*   Updated: 2024/06/14 18:04:34 by arlarzil         ###   ########.fr       */
+/*   Updated: 2024/08/07 14:43:48 by arlarzil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	print_parse_err(const char *tok)
 {
-	ft_putstr_fd("Parse error near token: '", 1);
-	ft_putstr_fd(tok, 1);
-	ft_putstr_fd("'\n", 1);
+	ft_putstr_fd("Parse error near token: '", 2);
+	ft_putstr_fd(tok, 2);
+	ft_putstr_fd("'\n", 2);
 }
